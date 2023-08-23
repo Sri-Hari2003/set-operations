71507a36f82e73d0a61d85003f2888cb8e57d9e8
Sri-Hari2003 <118734000+Sri-Hari2003@users.noreply.github.com>
2023-08-23 21:31:47 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+
+A = {0, 2, 4, 6, 8}
+B = {1, 2, 3, 4, 5}
+
+union_result = A.union(B)
+intersection_result = A.intersection(B)
+difference_result = A.difference(B)
+symmetric_difference_result = A.symmetric_difference(B)
+
+print("Union of A and B is", union_result)
+print("Intersection of A and B is", intersection_result)
+print("Difference of A and B is", difference_result)
+print("Symmetric difference of A and B is", symmetric_difference_result)
